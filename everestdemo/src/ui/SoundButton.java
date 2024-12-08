a5aa1fe2715cfa9988b2eb8321da1455e59c416f
fictionithink <139228422+fictionithink@users.noreply.github.com>
2024-12-08 18:05:38 +0800

    Update SoundButton.java

--- everestdemo/src/ui/SoundButton.java
+++ everestdemo/src/ui/SoundButton.java
@@ -1,7 +1,6 @@
 package ui;
 
 import java.awt.Graphics;
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 import utils.LoadSave;
@@ -62,7 +61,4 @@ public class SoundButton extends PauseButton {
 
     public void setMuted(boolean muted) {this.muted = muted;}
 
-    public void mousePressed(MouseEvent e) {
-
-    }
 }
