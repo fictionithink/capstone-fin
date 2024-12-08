f239210e298c2c151f4571292f503c0be3054565
fictionithink <139228422+fictionithink@users.noreply.github.com>
2024-12-08 18:06:30 +0800

    Update HelpMethods.java

--- everestdemo/src/utils/HelpMethods.java
+++ everestdemo/src/utils/HelpMethods.java
@@ -74,7 +74,7 @@ public class HelpMethods {
     }
 
     public static boolean isFloor(Rectangle2D.Float hitbox, float xSpeed,int[][] lvlData){
-        if(xSpeed > 0)
+        if(xSpeed >0)
             return IsSolid(hitbox.x+ hitbox.width,hitbox.y +hitbox.height + 1,lvlData);
         else
             return IsSolid(hitbox.x+xSpeed,hitbox.y +hitbox.height + 1,lvlData);
