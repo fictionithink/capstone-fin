b79a9e07e15800591abb983b9cae08161122f243
fictionithink <139228422+fictionithink@users.noreply.github.com>
2024-12-15 12:45:26 +0800

    Update About.java

--- everestdemo/src/gamestates/About.java
+++ everestdemo/src/gamestates/About.java
@@ -13,8 +13,8 @@
 
     public class About extends State implements Statemethods {
 
-        private UrmButton menuB; // Back button
-        private BufferedImage aboutP; // About page image
+        private UrmButton menuB;
+        private BufferedImage aboutP;
         private int menuX, menuY, menuWidth, menuHeight;
 
         public About(Game game) {
@@ -24,37 +24,29 @@
         }
 
         private void loadBackImg() {
-            // Load the about page image
             aboutP = LoadSave.getSpriteAtlas(LoadSave.ABOUT_PAGE);
             menuWidth = (int) (aboutP.getWidth() * Game.SCALE);
             menuHeight = (int) (aboutP.getHeight() * Game.SCALE);
         }
 
         private void loadButton() {
-            // Assign to instance variables, not local variables
             this.menuX = (int) (403 * Game.SCALE);
-            this.menuY = (int) (400 * Game.SCALE); // Place it near the bottom
+            this.menuY = (int) (400 * Game.SCALE);
             //menuB = new UrmButton(menuX, menuY, URM_SIZE_MENU , URM_SIZE_MENU , 2, 6);
         }
 
         @Override
         public void update() {
-            // Update the menu button
             menuB.update();
         }
 
         @Override
         public void draw(Graphics g) {
-            // Draw the about page image
             g.drawImage(aboutP, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
-
-            // Draw the menu button
-            //menuB.drawMenu(g);
         }
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            // No specific functionality here
         }
 
         @Override
@@ -74,16 +66,14 @@
 
         @Override
         public void mouseReleased(MouseEvent e) {
-            // Handle the menu button release
             if (isIn(e, menuB) && menuB.isMousePressed()) {
-                Gamestate.state = Gamestate.MENU; // Go back to the main menu
+                Gamestate.state = Gamestate.MENU;
             }
             menuB.resetBools();
         }
 
         @Override
         public void mouseMoved(MouseEvent e) {
-            // Set hover effect for the menu button
             menuB.setMouseOver(false);
 
             if (isIn(e, menuB))
@@ -92,7 +82,6 @@
 
         @Override
         public void keyPressed(KeyEvent e) {
-            // Allow ESC key to go back to the main menu
             if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
                 Gamestate.state = Gamestate.MENU;
             }
@@ -100,7 +89,6 @@
 
         @Override
         public void keyReleased(KeyEvent e) {
-            // No specific functionality here
         }
 
         private boolean isIn(MouseEvent e, UrmButton b) {
